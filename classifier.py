34b08ebd2c0777f201b58f8675a168b8dda42aaa
clarkechris70 <clarkechris70@gwu.edu>
2017-04-04 01:11:55 -0400

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -6,43 +6,6 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import svm
 from sklearn.metrics import classification_report
 
-def WikiArticleClassifierTest():
-    print("WikiArticleClassifierTest:")
-    print("python %s <WikiData>" % sys.argv[0])
-
-if __name__ == '__main__':
-
-    if len(sys.argv) < 6:
-        WikiArticleClassifierTest()
-        sys.exit(1)
-
-    WikiData = sys.argv[1]
-    ArticleClasses = ['Historical', 'Celebrity', 'Geography', 'Academic', 'Business', 'Culture']
-    #Need to confirm what categories are being used
-
-    # Read data
-    # Need help with integrating data file into this
-    trainWikiData = []
-    trainWikiLabels = []
-    testWikiData = []
-    testWikiLabels = []
-    
-    for current in ArticleClasses:
-        WikiDirectory = os.path.join(WikiData, current)
-        for fileName in os.listdir(WikiDirectory):
-            with open(os.path.join(WikiDirectory, filename), 'r') as f:
-                content = f.read()
-            #Split data set into training (90% of documents) and testing (10% of documents) using file names
-            #If the file starts with "wikiX" for example, then it will run through files X=[1...9]
-                if filename.startswith('___'):
-                # 10% testing data
-                    testWikiData.append(content)
-                    testWikiLabels.append(current)
-                else:
-                # 90% training data
-                    trainWikiData.append(content)
-                    trainWikiLabels.append(current)
-
     # Produce feature vectors
     # Included here for my parameters - can remove or change/substitute
     vectorizer = TfidfVectorizer(min_df=6, max_df = 0.75, sublinear_tf=True, use_idf=True)
